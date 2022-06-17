4d8bc9ee2e53ce87778f603a33fecabed54af0c5
aosdinkim <jh.kim@evmobility.com>
2022-06-17 15:57:50 +0900

    수정

--- js/common.js
+++ js/common.js
@@ -28,7 +28,7 @@ let GAGIM = (function() {
         ttl.to('#bag', {duration: 0.5, scale: 0.6, left: '60%', top: '75%', marginLeft: -204, ease: 'ease', delay: 3});
         ttl.to('#notiBox', {duration: 0.5, alpha: 1, ease: 'Back.easeOut', delay: 5});
         ttl.to('#noti', {duration: 0.5, scale: 1, alpha: 1, ease: 'Back.easeOut'});
-        ttl.to('#bag', {duration: 0.5, left: '50%', top: '50%', scale: 0.8, marginLeft: -85, ease: 'ease', delay: -1.5});
+        ttl.to('#bag', {duration: 0.5, left: '50%', top: '50%', scale: 0.8, marginLeft: -85,marginTop: -111, ease: 'ease', delay: -1.5});
         ttl.to('#shadow', {duration: 1, scale: 1, alpha: 1, ease: 'Back.easeOut', delay: -1});
         ttl.to('#notiSmallSpot', {duration: 0.5, y: 0, alpha: 1, ease: 'Back.easeOut', delay: -0.5});
         ttl.to('#btnStep01', {duration: 0.5, alpha: 1, display:'flex', ease: 'ease', delay: -0.5});
@@ -54,7 +54,7 @@ let GAGIM = (function() {
         ctl.to('#smallBag', {duration: 0.3, alpha: 1, y: 0, ease: 'ease', delay: -0.3});
         ctl.to('#smallBag', {duration: 0.6, y: 5, yoyo: true, repeat: -1, delay: 0.5});
         ctl.to('#cPhoneBox', {duration: 0.3, autoAlpha: 0, delay: 1.5});
-        ctl.to('#cBox', {height: '0%', duration: 0.5, delay: 0});
+        ctl.to('#cBox', {height: '0%', duration: 1, delay: 0});
     }
     _public.motion02 = function () {
         txtTl.to('#mainTit', {duration: 0.5, text: '상세 내용을 조율하고', ease: 'ease', delay: 0})
@@ -67,21 +67,21 @@ let GAGIM = (function() {
         ttl.to('#btnStep02', {duration: 0.5, alpha: 0, display:'none', ease: 'ease', delay: 2, oncomplete: _public.motion03});
 
         ctl.to('#cBox', {height: '120%', duration: 2, delay: 2.2});
-        ctl.to('#cBox', {height: '0', duration: 1, delay: 1.2});
     }
     _public.motion03 = function () {
-        txtTl.to('#mainTit', {duration: 0.5, text: '약속 시간에 맞춰<br>미션을 수행해요', ease: 'ease', delay: 0})
+        ctl.to('#cBox', {height: '0', duration: 1, delay: 2.1});
+        txtTl.to('#mainTit', {duration: 0.5, text: '약속 시간에 맞춰<br>미션을 수행해요', ease: 'ease', delay: 1.5})
         txtTl.to('#mainTit', {duration: 0.5, text: '미션 수행 후<br>사진으로 인증하면', ease: 'ease', delay: 4.5})
         txtTl.to('#mainTit', {duration: 0.3, text: '에코코인이 지급되어요 :)', ease: 'ease', delay: 3.5})
 
         ttl.to('#btnStep02', {duration: 0.5, alpha: 0, display:'none', ease: 'ease', delay: -1.5});
-        ttl.to('#chatBox', {duration: 1, autoAlpha: 0, display: 'none', ease: 'ease', delay: -0.5});
+        ttl.to('#chatBox', {duration: 1, autoAlpha: 0, display: 'none', ease: 'ease', delay: 1});
         ttl.to('#movBg', {duration: 1, autoAlpha: 1, display: 'block', ease: 'ease', delay: -1});
         ttl.to('#movingBg', {duration: 1, autoAlpha: 1, display: 'block', ease: 'ease', delay: -1});
         ttl.to('#bag', {duration: 0.5, autoAlpha: 1, scale:1.2, display: 'block', ease: 'Back.easeOut', delay: -1});
         ttl.to('#movBox', {duration: 1, autoAlpha: 0, ease: 'ease', delay: 3});
-        ttl.to('#yellowShadow', {duration: 0.3, autoAlpha:1, scale: 1, ease: 'Back.easeOut', delay: 1});
-        ttl.to('#bag', {duration: 0.5, x: 30, y: -30, scale:0.7, ease: 'ease', delay: -1.5});
+        ttl.to('#yellowShadow', {duration: 0.5, autoAlpha:1, scale: 1, ease: 'Back.easeOut', delay: 0});
+        ttl.to('#bag', {duration: 0.3, x: 30, y: -30, scale:0.7, ease: 'ease', delay: -0.5});
         ttl.to('#camera', {duration: 0.5, autoAlpha:1, ease: 'ease', delay: -1});
         ttl.to('#focus', {duration: 0.5, autoAlpha:1, scale: 1, ease: 'Back.easeOut', delay: 0});
         ttl.to('#cameraBag', {duration: 3, autoAlpha:1, ease: 'ease', delay: 0});
